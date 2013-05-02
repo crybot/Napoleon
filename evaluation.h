4062bd7f37a4bc7e3ed5250ca12fb018a93d0f6e
Marco <crybot@hotmail.it>
2013-05-03 00:05:35 +0300

    Update evaluation.h
    Added windows support

--- evaluation.h
+++ evaluation.h
@@ -6,82 +6,83 @@
 #include "board.h"
 namespace Napoleon
 {
-    extern int pawnSquareValue[64];
-    extern int knightSquareValue[64];
-    extern int bishopSquareValue[64];
-    extern int queenSquareValue[64];
-    extern int kingSquareValue[64];
 
     class Evaluation
-    {
-    public:
-        static int Evaluate(Board&);
-        static int EvaluatePiece(Byte, Byte, int);
+	{
+	public:
+		static int Evaluate(Board&);
+		static int EvaluatePiece(Byte, Byte, int);
 
-    private:
-        template<Byte>
-        static int evaluateMobility(Board&, BitBoard);
+	private:
+		template<Byte>
+		static int evaluateMobility(Board&, BitBoard);
 
-    };
+		static int pawnSquareValue[64];
+		static int knightSquareValue[64];
+		static int bishopSquareValue[64];
+		static int queenSquareValue[64];
+		static int kingSquareValue[64];
 
-
-    INLINE int Evaluation::Evaluate(Board & board)
-    {
-        using namespace Utils::BitBoard;
-
-        int material =
-                +   2000*(board.NumOfPieces[PieceColor::White][PieceType::King] - board.NumOfPieces[PieceColor::Black][PieceType::King])
-                +   900*(board.NumOfPieces[PieceColor::White][PieceType::Queen] - board.NumOfPieces[PieceColor::Black][PieceType::Queen])
-                +   500*(board.NumOfPieces[PieceColor::White][PieceType::Rook] - board.NumOfPieces[PieceColor::Black][PieceType::Rook])
-                +   330*(board.NumOfPieces[PieceColor::White][PieceType::Bishop] - board.NumOfPieces[PieceColor::Black][PieceType::Bishop])
-                +   320*(board.NumOfPieces[PieceColor::White][PieceType::Knight] - board.NumOfPieces[PieceColor::Black][PieceType::Knight])
-                +   100*(board.NumOfPieces[PieceColor::White][PieceType::Pawn] - board.NumOfPieces[PieceColor::Black][PieceType::Pawn]);
+	};
 
 
-        int wM = 0;
-        int bM = 0;
+	INLINE int Evaluation::Evaluate(Board & board)
+	{
+		using namespace Utils::BitBoard;
 
-        for (int i=0; i<64; i++)
-        {
-            if (board.PieceSet[i].Color != PieceColor::None)
-            {
-                Piece piece = board.PieceSet[i];
+		int material =
+			+   2000*(board.NumOfPieces[PieceColor::White][PieceType::King] - board.NumOfPieces[PieceColor::Black][PieceType::King])
+			+   900*(board.NumOfPieces[PieceColor::White][PieceType::Queen] - board.NumOfPieces[PieceColor::Black][PieceType::Queen])
+			+   500*(board.NumOfPieces[PieceColor::White][PieceType::Rook] - board.NumOfPieces[PieceColor::Black][PieceType::Rook])
+			+   330*(board.NumOfPieces[PieceColor::White][PieceType::Bishop] - board.NumOfPieces[PieceColor::Black][PieceType::Bishop])
+			+   320*(board.NumOfPieces[PieceColor::White][PieceType::Knight] - board.NumOfPieces[PieceColor::Black][PieceType::Knight])
+			+   100*(board.NumOfPieces[PieceColor::White][PieceType::Pawn] - board.NumOfPieces[PieceColor::Black][PieceType::Pawn]);
 
-                if(board.PieceSet[i].Color == PieceColor::White)
-                {
-                    wM += EvaluatePiece(piece.Type, piece.Color, i);
-                }
-                else
-                {
-                    bM += EvaluatePiece(piece.Type, piece.Color, i);
-                }
-            }
-        }
 
-        return (material + (wM - bM)) * (1-(board.SideToMove*2));
-    }
+		int wM = 0;
+		int bM = 0;
 
-    INLINE int Evaluation::EvaluatePiece(Byte piece, Byte color, int square)
-    {
-        using namespace Utils::Square;
-        switch(piece)
-        {
-        case PieceType::Pawn:
-            return color == PieceColor::White ? pawnSquareValue[square] : pawnSquareValue[GetSquareIndex(square % 8, 7 - (square/8))];
+		for (int i=0; i<64; i++)
+		{
+			if (board.PieceSet[i].Color != PieceColor::None)
+			{
+				Piece piece = board.PieceSet[i];
 
-        case PieceType::Knight:
-            return color == PieceColor::White ? knightSquareValue[square] : knightSquareValue[GetSquareIndex(square % 8, 7 - (square/8))];
+				if(board.PieceSet[i].Color == PieceColor::White)
+				{
+					wM += EvaluatePiece(piece.Type, piece.Color, i);
+				}
+				else
+				{
+					bM += EvaluatePiece(piece.Type, piece.Color, i);
+				}
+			}
+		}
 
-        case PieceType::Bishop:
-            return color == PieceColor::White ? bishopSquareValue[square] : bishopSquareValue[GetSquareIndex(square % 8, 7 - (square/8))];
+		return (material + (wM - bM)) * (1-(board.SideToMove*2));
+	}
 
-        case PieceType::King:
-            return color == PieceColor::White ? kingSquareValue[square] : kingSquareValue[GetSquareIndex(square % 8, 7 - (square/8))];
+	INLINE int Evaluation::EvaluatePiece(Byte piece, Byte color, int square)
+	{
+		using namespace Utils::Square;
+		switch(piece)
+		{
+		case PieceType::Pawn:
+			return color == PieceColor::White ? pawnSquareValue[square] : pawnSquareValue[GetSquareIndex(square % 8, 7 - (square/8))];
 
-        }
+		case PieceType::Knight:
+			return color == PieceColor::White ? knightSquareValue[square] : knightSquareValue[GetSquareIndex(square % 8, 7 - (square/8))];
 
-        return 0;
-    }
+		case PieceType::Bishop:
+			return color == PieceColor::White ? bishopSquareValue[square] : bishopSquareValue[GetSquareIndex(square % 8, 7 - (square/8))];
+
+		case PieceType::King:
+			return color == PieceColor::White ? kingSquareValue[square] : kingSquareValue[GetSquareIndex(square % 8, 7 - (square/8))];
+
+		}
+
+		return 0;
+	}
 }
 
 #endif // EVALUATION_H
